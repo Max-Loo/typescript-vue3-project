812ddb6c557d2277b2e3d80bb93261da9ebe7a8f
Max-Loo <luyuexing.scut@qq.com>
2021-02-22 09:44:54 +0800

    fix: 添加代码格式配置

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,24 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <!-- <div>{{ title }}</div> -->
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { defineComponent } from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 export default defineComponent({
-  name: "Home",
+  name: 'Home',
+  data() {
+    return {
+      title: '',
+      a: '',
+    }
+  },
   components: {
-    HelloWorld
-  }
-});
+    HelloWorld,
+  },
+})
 </script>
